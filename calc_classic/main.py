809e4408f4dcb8e977e768610be7689004563942
nuretdinov <nured@yandex.ru>
2023-11-28 22:47:10 +0300

    поправил по pep8

--- calc_classic/main.py
+++ calc_classic/main.py
@@ -1,46 +1,54 @@
+from tkinter import *
+
 num1 = 0
 num2 = 0
-mathSing = ''
+math_sing = ''
+
 
 def num(x):
-    ent_Calc.insert(END, x)
+    ent_calc.insert(END, x)
+
 
 def sign(z):
-    global num1
-    global mathSing
-    num1 = ent_Calc.get()
-    ent_Calc.delete(0, END)
-    mathSing = z
+    global num1, math_sing
+    num1 = ent_calc.get()
+    ent_calc.delete(0, END)
+    math_sing = z
+
 
 def clr():
-    ent_Calc.delete(0, END)
+    ent_calc.delete(0, END)
+
 
 def equal():
-    global num1
-    global num2
-    global mathSing
-    num2 = ent_Calc.get()
-    ent_Calc.delete(0, END)
+    global num1, num2, math_sing
+    num2 = ent_calc.get()
+    ent_calc.delete(0, END)
     num1 = int(num1)
     num2 = int(num2)
-    match mathSing:
-        case "+": result = num1 + num2
-        case "-": result = num1 - num2
-        case "*": result = num1 * num2
+    match math_sing:
+        case "+":
+            result = num1 + num2
+        case "-":
+            result = num1 - num2
+        case "*":
+            result = num1 * num2
         case "/":
-            try: result = round(num1 / num2, 2)
-            except ZeroDivisionError: result = 'Ошибка, 0!'
+            try:
+                result = round(num1 / num2, 2)
+            except ZeroDivisionError:
+                result = 'Ошибка, 0!'
         case _:
             result = 'Ошибка, знак!'
-    ent_Calc.delete(0, END)
-    ent_Calc.insert(0, result)
+    ent_calc.delete(0, END)
+    ent_calc.insert(0, result)
 
-from tkinter import *
+
 root = Tk()
 root.geometry('170x210+200+200')
 root.title('Калькулятор')
-ent_Calc = Entry(root, width=14, font='Arial 12')
-ent_Calc.grid(row=1, column=1, columnspan=4, pady=5, padx=5)
+ent_calc = Entry(root, width=14, font='Arial 12')
+ent_calc.grid(row=1, column=1, columnspan=4, pady=5, padx=5)
 Button(root, text=' 7 ', font='Arial 12', command=lambda: num(7)).grid(row=2, column=1, pady=5, padx=5)
 Button(root, text=' 8 ', font='Arial 12', command=lambda: num(8)).grid(row=2, column=2, pady=5, padx=5)
 Button(root, text=' 9 ', font='Arial 12', command=lambda: num(9)).grid(row=2, column=3, pady=5, padx=5)
