3fefacce8b2a33286c193d94841b2211762950e1
nuretdinov <nured@yandex.ru>
2023-11-28 22:51:46 +0300

    pep8

--- calc/main.py
+++ calc/main.py
@@ -1,26 +1,34 @@
 def calc():
-    num1 = EntryA.get()
-    num2 = EntryB.get()
-    mathSing = EntryC.get()
+    num1 = ent_num1.get()
+    num2 = ent_num2.get()
+    math_sing = ent_math_sign.get()
 
-    if num1 != '' and num2 != '' and mathSing != '':
+    if num1 != '' and num2 != '' and math_sing != '':
         try:
             num1 = float(num1)
             num2 = float(num2)
-            match mathSing:
-                case "+": result = round(num1 + num2)
-                case "-": result = round(num1 - num2)
-                case "*": result = round(num1 * num2)
+            match math_sing:
+                case "+":
+                    result = round(num1 + num2)
+                case "-":
+                    result = round(num1 - num2)
+                case "*":
+                    result = round(num1 * num2)
                 case "/":
-                    if num2 != 0: result = round(num1 / num2, 2)
-                    else: result = 'Ошибка, 0!'
-                case _: result = 'Ошибка, знак!'
-        except ValueError: result = 'Ошибка, числа!'
+                    if num2 != 0:
+                        result = round(num1 / num2, 2)
+                    else:
+                        result = 'Ошибка, 0!'
+                case _:
+                    result = 'Ошибка, знак!'
+        except ValueError:
+            result = 'Ошибка, числа!'
     else:
         result = 'Ошибка, поля!'
 
-    Result.delete(0, END)
-    Result.insert(0, result)
+    ent_result.delete(0, END)
+    ent_result.insert(0, result)
+
 
 from tkinter import *
 
@@ -28,16 +36,16 @@ root = Tk()
 root.geometry('300x300+200+200')
 root.title('Калькулятор')
 Label(root, text='Первое число', font='Arial 12', height=2).pack()
-EntryA = Entry(root, width=10, font='Arial 12')
-EntryA.pack()
+ent_num1 = Entry(root, width=10, font='Arial 12')
+ent_num1.pack()
 Label(root, text='Второе число', font='Arial 12', height=2).pack()
-EntryB = Entry(root, width=10, font='Arial 12')
-EntryB.pack()
+ent_num2 = Entry(root, width=10, font='Arial 12')
+ent_num2.pack()
 Label(root, text='Знак действия', font='Arial 12', height=2).pack()
-EntryC = Entry(root, width=10, font='Arial 12')
-EntryC.pack()
+ent_math_sign = Entry(root, width=10, font='Arial 12')
+ent_math_sign.pack()
 Button(root, text='     =     ', font='Arial 12', command=calc).pack()
 Label(root, text='Результат', font='Arial 12', height=2).pack()
-Result = Entry(root, width=14, font='Arial 12')
-Result.pack()
+ent_result = Entry(root, width=14, font='Arial 12')
+ent_result.pack()
 root.mainloop()
