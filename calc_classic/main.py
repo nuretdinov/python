12b8e54a036aa88c1b56a8635bc2d2c52e9214f9
nuretdinov <nured@yandex.ru>
2023-11-28 21:48:40 +0300

    some

--- calc_classic/main.py
+++ calc_classic/main.py
@@ -3,24 +3,24 @@ num2 = 0
 mathSing = ''
 
 def num(x):
-    EntryCalc.insert(END, x)
+    ent_Calc.insert(END, x)
 
 def sign(z):
     global num1
     global mathSing
-    num1 = EntryCalc.get()
-    EntryCalc.delete(0, END)
+    num1 = ent_Calc.get()
+    ent_Calc.delete(0, END)
     mathSing = z
 
 def clr():
-    EntryCalc.delete(0, END)
+    ent_Calc.delete(0, END)
 
 def equal():
     global num1
     global num2
     global mathSing
-    num2 = EntryCalc.get()
-    EntryCalc.delete(0, END)
+    num2 = ent_Calc.get()
+    ent_Calc.delete(0, END)
     num1 = int(num1)
     num2 = int(num2)
     match mathSing:
@@ -32,15 +32,15 @@ def equal():
             except ZeroDivisionError: result = 'Ошибка, 0!'
         case _:
             result = 'Ошибка, знак!'
-    EntryCalc.delete(0, END)
-    EntryCalc.insert(0, result)
+    ent_Calc.delete(0, END)
+    ent_Calc.insert(0, result)
 
 from tkinter import *
 root = Tk()
 root.geometry('170x210+200+200')
 root.title('Калькулятор')
-EntryCalc = Entry(root, width=14, font='Arial 12')
-EntryCalc.grid(row=1, column=1, columnspan=4, pady=5, padx=5)
+ent_Calc = Entry(root, width=14, font='Arial 12')
+ent_Calc.grid(row=1, column=1, columnspan=4, pady=5, padx=5)
 Button(root, text=' 7 ', font='Arial 12', command=lambda: num(7)).grid(row=2, column=1, pady=5, padx=5)
 Button(root, text=' 8 ', font='Arial 12', command=lambda: num(8)).grid(row=2, column=2, pady=5, padx=5)
 Button(root, text=' 9 ', font='Arial 12', command=lambda: num(9)).grid(row=2, column=3, pady=5, padx=5)
