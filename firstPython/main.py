45dc45c2466b816cab5f45bf70f64b3e4f0931b3
nuretdinov <nured@yandex.ru>
2023-11-29 10:28:07 +0300

    little update theory

--- firstPython/main.py
+++ firstPython/main.py
@@ -3,15 +3,26 @@ num2 = input('Введите второе число:')
 sum = int(num1) + int(num2)
 print('Сумма= ' + str(sum))
 
-# int, float, str
-# + - * / ** > < >= <= == != and or not
+# int, float, str, bool
+# + - * / % ** > < >= <= == != and or not (import math) math.sqrt()
+#1<2<3<4
+# print(type(x)), float(x) int(x) str(x)
 
-friends_list = ['Max', 'Ann', 'Roman']  # список
+#строки
+# обратится в элементу строки str[5], str[:5] - от начала до 5ог, str[5:] - с 5ого до конца, str[5:7] - с 5ого до 7ого, str[::2] - с шагом 2
+#len() count() find() split
+"""
+x=10
+y=20
+print(f'выводим строку c переменной {x} и {y}')
+"""
+
+friends_list = ['Max', 'Ann', 'Roman']  # список - list - могут быть разные типы данных элементов
 print(friends_list[2])
 
-name_list = {'Max', 'Ann', 'Muhtar'}  # набор - неупорядоченный
-guests = {'Max': 'boy', 'Ann': 'girl', 'Roman': 'boy'}  # кортеж (словарь)
-
+name_list = {'Max', 'Ann', 'Muhtar'}  # set - множество - последовательность уникальных - неупорядоченный?
+guests = {'Max': 'boy', 'Ann': 'girl', 'Roman': 'boy'}  # dict - словарь - список пара-ключ
+# (10, 20, 'строка') - tuple - кортеж - последовательность неизменяемых объектов
 i = 0
 while i <= 10:
     print(i)
