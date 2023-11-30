d4590cd983603232ba3e49857223161053e57956
nuretdinov <nured@yandex.ru>
2023-11-30 10:10:16 +0300

    ver 1.0

--- myFirstGame/main_oop.py
+++ myFirstGame/main_oop.py
@@ -9,13 +9,24 @@ pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Older Superman in a galaxy far, far away")
 clock = pygame.time.Clock()
-
-# добавляем музыку
 pygame.mixer.music.load("static/music/music.mp3")
 pygame.mixer.music.play()
 blow_sound = pygame.mixer.Sound("static/music/blow_sound.mp3")
+background = pygame.image.load("static/img/bg.jpg").convert()
+background_rect = background.get_rect()
+alien_imgs = []
+alien_list = ['static/img/alien.png', 'static/img/alien1.png', 'static/img/alien2.png', ]
+for img in alien_list:
+    alien_imgs.append(pygame.image.load(img))
+# заставка
+start_screen = pygame.image.load("static/img/start.jpg").convert()
+screen.blit(start_screen, (0, 0))
+start_text = pygame.font.Font(None, 24)
+start_text_render = start_text.render('[..... press space .....]', True, (180, 180, 0))
+screen.blit(start_text_render, (300, 420))
+pygame.display.update()
 
-class Player(pygame.sprite.Sprite):
+class Hero(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load("static/img/hero.png")
@@ -50,10 +61,10 @@ class Player(pygame.sprite.Sprite):
             self.rect.top = 0
 
 
-class Mob(pygame.sprite.Sprite):
+class Alien(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("static/img/alien.png")
+        self.image = random.choice(alien_imgs)
         self.rect = self.image.get_rect()
         self.rect.x = WIDTH + self.rect.width
         self.rect.y = random.randrange(50, 550)
@@ -67,7 +78,7 @@ class Mob(pygame.sprite.Sprite):
             self.rect.x = WIDTH + self.rect.width
             self.rect.y = random.randrange(50, 550)
             self.speedy = random.randrange(1, 10)
-            alien_score +=1
+            alien_score += 1
 
 
 class Blow(pygame.sprite.Sprite):
@@ -85,54 +96,55 @@ class Blow(pygame.sprite.Sprite):
             self.kill()
 
 
-all_sprites = pygame.sprite.Group()
-player = Player()
-all_sprites.add(player)
-mobs = pygame.sprite.Group()
-def add_mobs(num):
+def add_aliens(num):
     for i in range(num):
-        m = Mob()
-        all_sprites.add(m)
-        mobs.add(m)
-add_mobs(10)
+        new_alien = Alien()
+        all_sprites.add(new_alien)
+        alians.add(new_alien)
+
+
+all_sprites = pygame.sprite.Group()
+hero = Hero()
+all_sprites.add(hero)
+alians = pygame.sprite.Group()
+add_aliens(10)
 blows = pygame.sprite.Group()
 
-# фон
-background = pygame.image.load("static/img/bg.jpg").convert()
-background_rect = background.get_rect()
-
-running = True
 hero_score = 0
 alien_score = 0
+running = True
+start = False
 
 while running:
 
-    clock.tick(FPS)
+    # заставка ждет нажатия пробела
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                start = True
 
-    hits = pygame.sprite.spritecollide(player, mobs, True)
-    for hit in hits:
-        blow = Blow(hit.rect.center)
-        all_sprites.add(blow)
-        blows.add(blow)
-        blow_sound.play()
-        add_mobs(3)
-        hero_score += 1
+    if start:
 
-    screen.blit(background, background_rect)
-    all_sprites.update()
-    all_sprites.draw(screen)
-    pygame.display.update()
+        clock.tick(FPS)
 
-    # не выводится !
-    score_title_font = pygame.font.Font(None, 32)
-    score_font = pygame.font.Font(None, 28)
-    score_title = score_title_font.render('Score', True, (180, 180, 0))
-    hero_score_text = score_font.render(f'Hero: {hero_score}', True, (180, 180, 0))
-    alien_score_text = score_font.render(f'Alien: {alien_score}', True, (180, 180, 0))
-    screen.blit(score_title, (20, 510))
-    screen.blit(hero_score_text, (20, 540))
-    screen.blit(alien_score_text, (20, 560))
+        hits = pygame.sprite.spritecollide(hero, alians, True)
+        for hit in hits:
+            blow = Blow(hit.rect.center)
+            all_sprites.add(blow)
+            blows.add(blow)
+            blow_sound.play()
+            add_aliens(3)
+            hero_score += 1
 
+        screen.blit(background, background_rect)
+        all_sprites.update()
+        all_sprites.draw(screen)
+
+        score_font = pygame.font.Font(None, 22)
+        score_text = score_font.render(f'[{hero_score}] hero VS alien [{alien_score}]', True, (180, 180, 0))
+        screen.blit(score_text, (20, 560))
+
+        pygame.display.update()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
