401de55974c0251b171676a3cba459e7dcc66fd2
nuretdinov <nured@yandex.ru>
2023-12-11 20:45:03 +0300

    tg bot ver. 0.2

--- clickerTest/main.py
+++ clickerTest/main.py
@@ -1,5 +1,5 @@
 import pygame
-import time
+from tkinter import messagebox
 
 W = 640
 H = 480
@@ -8,37 +8,69 @@ pygame.init()
 screen = pygame.display.set_mode((W, H))
 pygame.display.set_caption("Test click speed")
 clock = pygame.time.Clock()
+# храним количество кликов
 clicks_num = 0
+
 running = True
+# переменная для заставки и остановки по таймеру
+start = False
+# для времени, чтобы компенсировать паузу при заставке
+pause = 0
 
 while running:
 
     clock.tick(FPS)
 
+    # время в секундах
+    time = pygame.time.get_ticks() // 1000 - pause
+    # для остановки на 30 сек
+    if time >= 5:
+        if start:
+            messagebox.showinfo('Finished', f'You made {clicks_num} clicks')
+            clicks_num = 0
+            start = False
+
+    if not start:
+        # заставка
+        start_text = pygame.font.Font(None, 40)
+        start_text_render = start_text.render('- SPACE TO START CLICKS! -', True, (255, 255, 255))
+        screen.blit(start_text_render, (130, 220))
+        pygame.display.update()
+    else:
+        # выводим количество кликов когад играем
+        score_font = pygame.font.Font(None, 522)
+        score_text = score_font.render(str(clicks_num), True, (255, 255, 0))
+        if clicks_num < 10:
+            posX = W / 3
+        elif 10 < clicks_num < 100:
+            posX = W / 4 - 50
+        else:
+            posX = W / 5 - 120
+        screen.blit(score_text, (posX, 70))
+        # таймер
+        clock_font = pygame.font.Font(None, 80)
+        clock_text = clock_font.render(str(time), True, (255, 255, 255))
+        screen.blit(clock_text, (10, 10))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         else:
-            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                clicks_num += 1
+            # старт игры при заставке
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    # запускаем игру
+                    start = True
+                    # устанавливаем значение паузы пока была заставка, чтобы таймер шел с нуля
+                    pause = pygame.time.get_ticks() // 1000
 
-    screen.fill((0,0,0))
-
-    time = pygame.time.get_ticks()/1000
-    clock_font = pygame.font.Font(None, 80)
-    clock_text = clock_font.render(str(time), True, (255, 255, 255))
-    screen.blit(clock_text, (10, 10))
-
-    score_font = pygame.font.Font(None, 522)
-    score_text = score_font.render(str(clicks_num), True, (255, 255, 0))
-    if clicks_num < 10:
-        posX = W/3
-    elif 10 < clicks_num < 100:
-        posX = W/4 - 50
-    else:
-        posX = W/5 - 120
-    screen.blit(score_text, (posX, 70))
+            if start:
+                # считаем клики, отслеживаем нажатие кнопок мыши (левой кнопки)
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                    clicks_num += 1
 
     pygame.display.update()
+    screen.fill((0, 0, 0))
+
 
 pygame.quit()
--- tgBot/main.py
+++ tgBot/main.py
@@ -1,5 +1,6 @@
 from aiogram import Bot, Dispatcher, executor, types
 import requests, json
+# токены телеграм и яндекс.погоды
 import tg_token, ya_token
 
 TG_TOKEN = tg_token.TG_TOKEN
@@ -7,29 +8,49 @@ TG_TOKEN = tg_token.TG_TOKEN
 bot = Bot(token=TG_TOKEN)
 dp = Dispatcher(bot)
 
-
-@dp.message_handler(commands=['start'])
-async def send_welcome(message: types.Message):
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    buttons = ["🌡️ погода", "/bye"]
-    keyboard.add(*buttons)
-    await message.reply("Привет 🥰 ! Я версия SpeakToMe 1.0\n", reply_markup=keyboard)
-
-@dp.message_handler(lambda message: message.text == "🌡️ погода")
-async def weather_show(message: types.Message):
-    latitude = '59.9386'
-    longitude = '30.3141'
+# получаем погоду от яндекса
+def get_weather(lat, lon):
+    latitude = lat
+    longitude = lon
     url_yandex = f'https://api.weather.yandex.ru/v2/forecast?lat={latitude}&lon={longitude}&[lang=ru_RU]'
     yandex_req = requests.get(url_yandex, headers={'X-Yandex-API-Key': ya_token.YA_TOKEN}, verify=False)
     yandex_json = json.loads(yandex_req.text)
-    weather_out = 'Температура: ' + str(yandex_json['fact']['temp']) + "\nВетер: " + str(
-        yandex_json['fact']['wind_speed']) + ' м/с'
-    await message.reply(weather_out)
+    wind = {"nw":"северо-западный", "n":"северный", "ne":"северо-восточный", "e":"восточный", "se":"юго-восточный", "s":"южный", "sw":"юго-западный", "w":"западный", "c":"штиль"}
+    weather = 'Температура: ' + str(yandex_json['fact']['temp']) + "\nВетер: " + str(
+        yandex_json['fact']['wind_speed']) + ' м/с ' + str(wind[yandex_json['fact']['wind_dir']])
+    return weather
 
+# формируем общее меню с 2мя кнопками
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    buttons = ["🌡️ СПб", "🌡️ Мск"]
+    keyboard.add(*buttons)
+    await message.reply("Привет 🥰 ! Я версия SpeakToMe 1.0\n Сейчас могу показать погоду...", reply_markup=keyboard)
+
+# обработка нажатия на кнопки СПб
+@dp.message_handler(lambda message: message.text == "🌡️ СПб")
+async def weather_show(message: types.Message):
+    weather = get_weather(59.9386, 30.3141)
+    await message.reply(weather)
+
+# обработка нажатия на кнопки Мск
+@dp.message_handler(lambda message: message.text == "🌡️ Мск")
+async def weather_show(message: types.Message):
+    weather = get_weather(55.7522, 37.6156)
+    await message.reply(weather)
+
+# обработка команды bye
 @dp.message_handler(commands=['bye'])
 async def send_welcome(message: types.Message):
     await message.reply("До новых встреч\n 🤗🤗🤗 ")
 
+# обработка команды help
+@dp.message_handler(commands=['help'])
+async def send_welcome(message: types.Message):
+    await message.reply("/start - начало работы\n /help - список команд\n /bye - конец работы\n")
+
+# обработка любого текста от пользователя (неизместного боту)
 @dp.message_handler()
 async def echo(message: types.Message):
     await message.answer("Я еще не знаю команду '" + message.text + "'")
